4d373eb0eeb815139f594034c329050a20d314ef
Allen Whearry <allen@codeinecoding.com>
2019-02-02 22:56:37 -0500

    create beer photo cell

---
+++ StoreAppConceptAnimation/Controller/AddToCartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddToCartViewController.swift
+//  StoreAppConceptAnimation
+//
+//  Created by Allen Whearry on 2/2/19.
+//  Copyright © 2019 Codeine Technologies LLC. All rights reserved.
+//
+
+import UIKit
+
+class AddToCartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- StoreAppConceptAnimation/View/BeerPhotoCell.swift
+++ StoreAppConceptAnimation/View/BeerPhotoCell.swift
@@ -9,5 +9,34 @@
 import UIKit
 
 class BeerPhotoCell: UICollectionViewCell {
+    let beerImage: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.image = UIImage(named: "sw-header-logo")
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    fileprivate func setupView() {
+        addSubview(beerImage)
+        displayConstraints()
+    }
+    
+    fileprivate func displayConstraints() {
+        NSLayoutConstraint.activate([
+            beerImage.topAnchor.constraint(equalTo: topAnchor, constant: 32),
+            beerImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32),
+            beerImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
+            beerImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
